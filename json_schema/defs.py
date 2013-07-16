b953a3ed024cc15b746c9f5e4cd37bf6cb2b9773
Caleb Smith <caleb.smithnc@gmail.com>
2013-07-16 09:14:55 -0400

    Fixed some style quibbles in json_schema.defs

--- json_schema/defs.py
+++ json_schema/defs.py
@@ -2,6 +2,7 @@
 
 from json_schema.tokens import StringToken, IntegerToken, MetaToken
 
+
 class UnsignedIntegerToken(IntegerToken):
     def validate(self, value):
         return super(UnsignedIntegerToken, self).validate(value) \
@@ -12,5 +13,10 @@ class UInt32Token(UnsignedIntegerToken):
         return super(UInt32Token, self).validate(value) \
           and value <= 2 ** 32
 
-class AnyString(MetaToken): matching_token_types = (StringToken,)
-class AnyInteger(MetaToken): matching_token_types = (IntegerToken,)
+
+class AnyString(MetaToken):
+    matching_token_types = (StringToken,)
+
+
+class AnyInteger(MetaToken):
+    matching_token_types = (IntegerToken,)
